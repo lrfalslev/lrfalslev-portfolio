881579b72ad8cc601dbf3dd7b0ef55afbeed0ed6
lrfalslev <lrfalslev@gmail.com>
2025-01-28 14:20:15 -0700

    finish resume, make responsive

--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -3,17 +3,14 @@
     let filename = "LucasFalslevResume" + date + ".pdf";
 </script>
 
-<div class="w-2/3 mt-3 max-h-screen overflow-y-auto bg-primary">
-    <div class="py-4 px-28">   
-        <h1 class="text-4xl text-center">Lucas Falslev</h1>
+<div class="w-[95%] md:w-5/6 lg:w-2/3 text-[8px] md:text-sm lg:text-base mt-3 max-h-screen overflow-y-auto bg-primary">
+    <div class="py-4 px-4 lg:px-8 xl:px-28">   
+        <h1 class="text-4xl text-center pb-2">Lucas Falslev</h1>
         <div class="container mx-auto w-full">
             <div class="inline-flex items-center justify-between w-full">
-                <span>lrfalslev@gmail.com</span>
-                <span>https://lrfalslev-portfolio.pages.dev/</span>
-            </div>
-            <div class="inline-flex items-center justify-between w-full">
+                <span>lrfalslev@gmail.com</span>                
                 <span>(385) 313-7895</span>
-                <span>linkedin.com/in/luke-falslev</span>
+                <a class="link" href="https://lrfalslev-portfolio.pages.dev">lrfalslev-portfolio.pages.dev</a>
             </div>
         </div>
         <div>
@@ -30,34 +27,31 @@
             <div>
                 <div class="container mx-auto w-full">
                     <div class="inline-flex items-center justify-between w-full">
-                        <span><b class="text-lg">Software Developer II</b>, J.B. Hunt</span>
+                        <span><b class="text-xs md:text-sm lg:text-lg">Software Developer II</b>, J.B. Hunt</span>
                         <span>Sept 2024 – Present</span>
                     </div>
                     <div class="inline-flex items-center justify-between w-full">
-                        <span><b class="text-lg">Software Developer I</b>, J.B. Hunt</span>
+                        <span><b class="text-xs md:text-sm lg:text-lg">Software Developer I</b>, J.B. Hunt</span>
                         <span>Dec 2022 – Sept 2024</span>
                     </div>
                 </div>
                 <span>Remote, USA</span>
                 <ul class="list-disc list-outside ms-8">
-                    <li>Added new nested data structure in request space to support International and Final Mile requests,
-                        maintained backwards compatibility for current processes/systems</li>
-                    <li>Ingested messages from ActiveMQ, Kafka and API calls; updated and added API endpoints to surface new
-                        order and request tracking information</li>
+                    <li>Added new nested data structure in request space to support International and Final Mile requests, maintained backwards compatibility for current processes/systems</li>
+                    <li>Ingested messages from ActiveMQ, Kafka and API calls; updated and added API endpoints to surface new order and request tracking information</li>
                     <li>Converted apps from Kubernetes YAML/Kustomize manifests to Helm Charts</li>
-                    <li>Collaborated with senior developers and architects to determine solutions and implemented them,
-                        assisted interns with onboarding and roadblocks</li>
+                    <li>Collaborated with senior developers and architects to determine solutions and implemented them, assisted interns with onboarding and roadblocks</li>
                     <li>Updated schemas to current API specifications, documented processes in team wikis</li>
                 </ul>
             </div>
             <div class="mt-2">
                 <div class="container mx-auto w-full">
                     <div class="inline-flex items-center justify-between w-full">
-                        <span><b class="text-lg">Jr. Software Architect</b>, Fox Pest Control</span>
+                        <span><b class="text-xs md:text-sm lg:text-lg">Jr. Software Architect</b>, Fox Pest Control</span>
                         <span>April 2022 – Dec 2022</span>
                     </div>
                     <div class="inline-flex items-center justify-between w-full">
-                        <span><b class="text-lg">Software Developer I</b>, Fox Pest Control</span>
+                        <span><b class="text-xs md:text-sm lg:text-lg">Software Developer I</b>, Fox Pest Control</span>
                         <span>Jun 2021 – Mar 2022</span>
                     </div>
                 </div>
@@ -73,7 +67,7 @@
             <div class="mt-2">
                 <div class="container mx-auto w-full">
                     <div class="inline-flex items-center justify-between w-full">
-                        <span><b class="text-lg">Route Sales Manager</b>, Fox Pest Control</span>
+                        <span><b class="text-xs md:text-sm lg:text-lg">Route Sales Manager</b>, Fox Pest Control</span>
                         <span>Summers 2017 – 2020</span>
                     </div>
                 </div>
@@ -90,7 +84,7 @@
             <div>
                 <div class="container mx-auto w-full">
                     <div class="inline-flex items-center justify-between w-full">
-                        <span><b class="text-lg">University of Utah</b>, Salt Lake City, UT</span>
+                        <span><b class="text-xs md:text-sm lg:text-lg">University of Utah</b>, Salt Lake City, UT</span>
                         <span>Graduated 2021</span>
                     </div>
                 </div>
@@ -105,27 +99,25 @@
             <div>
                 <div class="container mx-auto w-full">
                     <div class="inline-flex items-center justify-between w-full">
-                        <b class="text-lg">Personal Portfolio</b>
+                        <b class="text-xs md:text-sm lg:text-lg">Personal Portfolio</b>
                         <span>Sveltekit, Tailwind</span>
                     </div>
                 </div>
-                <a href="https://github.com/lrfalslev/lrfalslev-portfolio">https://github.com/lrfalslev/lrfalslev-portfolio</a>
+                <a class="link" href="https://github.com/lrfalslev/lrfalslev-portfolio">https://github.com/lrfalslev/lrfalslev-portfolio</a>
                 <ul class="list-disc list-outside ms-8">
                     <li>Designed and coded a responsive personal portfolio with Sveltekit</li>
-                    <li>Automated deployment to cloudflare with github actions</li>
                 </ul>
             </div>
             <div class="mt-2">
                 <div class="container mx-auto w-full">
                     <div class="inline-flex items-center justify-between w-full">
-                        <b class="text-lg">Zombiez Game</b>
+                        <b class="text-xs md:text-sm lg:text-lg">Zombiez Game</b>
                         <span>Lua, Love2D</span>
                     </div>
                 </div>
-                <a href="https://github.com/coledtaylor/hackathon">https://github.com/coledtaylor/hackathon</a>
+                <a class="link" href="https://github.com/coledtaylor/hackathon">https://github.com/coledtaylor/hackathon</a>
                 <ul class="list-disc list-outside ms-8">
-                    <li>Constructed a functional 2D top down shooter as part of a team for a USU Hackathon</li>
-                    <li>Built game world, managed camera tracking and game state</li>
+                    <li>Built a 2D top down shooter as part of a team for a USU Hackathon</li>
                 </ul>
             </div>
         </div>

131400c5ce700624c33fe757c45a9b6ee19ae14a
lrfalslev <lrfalslev@gmail.com>
2025-01-29 01:36:15 -0700

    hero

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,13 +4,13 @@
     <div class="hero-content flex-col md:flex-row w-2/3">
         <div class="avatar md:me-4">
             <div class="w-60 h-96 rounded-2xl ring-gray-200 ring-offset-zinc-700 ring ring-offset-4">
-                <img class="size-24 object-right-top" src="luke-temp-profile.jpg" alt="Luke's headshot" />
+                <img class="scale-x-[-1] size-24 object-right-top" src="luke-temp-profile.jpg" alt="Luke's headshot" />
             </div>
         </div>
         <div class="font-extrabold">
             <h1 class="my-5 text-2xl md:text-4xl lg:text-5xl">Hello, I'm Luke!</h1>
             <p class="font-light md:py-6 md:text-2xl">
-                I'm a full stack software developer who likes elegant solutions. In my free time I enjoy cycling, snowboarding, and watching movies.
+                I'm a full stack software developer who likes elegant solutions. I enjoy cycling, good food, snowboarding, and watching movies.
             </p>
         </div>
     </div>

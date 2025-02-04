8e6fd3904a02945c736ab3469586401aefa30912
lrfalslev <lrfalslev@gmail.com>
2025-02-04 09:47:35 -0700

    lightbox improvements, add albums

--- src/routes/photography/+page.svelte
+++ src/routes/photography/+page.svelte
@@ -1,23 +1,6 @@
 <script lang="ts">
   import Lightbox from './components/Lightbox.svelte';
 
-  let items = [
-    'P1010819.jpg',
-    'P1010859.jpg',
-    'P1010879.jpg',
-    'P1010881.jpg',
-    'P1020990.jpg',
-    'P1030022.jpg',
-    'P1030085.jpg',
-    'P1030088.jpg',
-    'P1030162.jpg',
-    'P1030164.jpg',
-    'P1030167.jpg',
-    'P1030173.jpg',
-    'P1030243.jpg',
-    'P1030250.jpg',
-    'P1030257.jpg'];
-
   import img1 from '$lib/assets/pictures/P1010819.jpg';
   import img2 from '$lib/assets/pictures/P1010859.jpg';
   import img3 from '$lib/assets/pictures/P1010879.jpg';
@@ -34,26 +17,16 @@
   import img14 from '$lib/assets/pictures/P1030250.jpg';
   import img15 from '$lib/assets/pictures/P1030257.jpg';
 
-  let imgs = [
-    img1,
-    img2,
-    img3,
-    // img4,
-    img5,
-    img6,
-    img7,
-    img8,
-    img9,
-    img10,
-    img11,
-    img12,
-    img13,
-    img14,
-    img15,
+  let albumIdx = 0;
+  let albums = [
+    {description: "Farmington Station", images: [img1, img2, img3, img4]},
+    {description: "Old Granary", images: [img5, img6, img7, img8]},
+    {description: "USU Greenhouse", images: [img9, img10, img11, img12]},
+    {description: "Moab", images: [img13, img14, img15]}
   ];
   
-  function fullScreen (event: any) {
-      // const targetElement = event.target;
+  function openLightbox (idx: number) {
+      albumIdx = idx;
       const targetElement = document.getElementById('lightbox');
 
       var requestMethod = targetElement?.requestFullscreen;
@@ -64,7 +37,7 @@
     }
 </script>
 
-<Lightbox />
+<Lightbox bind:albumIdx={albumIdx} albums={albums}/>
 
 <div class="w-full max-h-screen overflow-y-auto bg-primary no-scrollbar">
   <div class="grid 
@@ -72,19 +45,18 @@
     gap-4       md:gap-8        lg:gap-12 
     py-4        md:py-8         lg:py-20 
     mx-4        md:mx-8         lg:mx-20">
-    {#each imgs as img, idx}
+    {#each albums as album, idx}
       <div class="flex items-center justify-center">
-        <button on:click={event => fullScreen(event)} class="card card-compact bg-base-300 shadow-xl">
+        <button on:click={_ => openLightbox(idx)} class="card card-compact bg-base-300 shadow-xl">
           <figure>
             {#if idx < 3}
-              <img src={img} alt="img" />
+              <img src={album.images[0]} alt="img" />
             {:else}
-              <img loading=lazy src={img} alt="img" />
+              <img loading=lazy src={album.images[0]} alt="img" />
             {/if}
           </figure>
           <div class="card-body">
-            <h2 class="card-title">Image name</h2>
-            <p>description</p>
+            <p>{album.description}</p>
           </div>
         </button>
       </div>
--- src/routes/photography/components/Lightbox.svelte
+++ src/routes/photography/components/Lightbox.svelte
@@ -1,49 +1,61 @@
 <script lang="ts">
-	import img1 from '$lib/assets/pictures/P1010819.jpg';
-	import img2 from '$lib/assets/pictures/P1010859.jpg';
-	import img3 from '$lib/assets/pictures/P1010879.jpg';
-	import img4 from '$lib/assets/pictures/P1010881.jpg';
-
-	let imgs = [img1, img2, img3, img4];
-
-	let idx = 0;
+	export let albumIdx: number;
+	export let albums: Array<any>; 
+	let imgIdx = 0;
 	let isFullScreen = false;
 
-	function iterate(change: number) {
-		idx += change;
-		if (idx == -1) {
-			idx = imgs.length - 1;
-		} else if (idx == imgs.length) {
-			idx = 0;
+	function imgIterate(change: number) {
+		imgIdx += change;
+		if (imgIdx == -1) {
+			imgIdx = albums[albumIdx].images.length - 1;
+		} else if (imgIdx == albums[albumIdx].images.length) {
+			imgIdx = 0;
 		}
 	}
 
-	function onKeyUp(e: any) {
+	function albumIterate(change: number) {
+		albumIdx += change;
+		if (albumIdx == -1) {
+			albumIdx = albums.length - 1;
+		} else if (albumIdx == albums.length) {
+			albumIdx = 0;
+		}
+	}
+
+	function onKeyDown(e: any) {
 		switch (e.key) {
 			case 'ArrowLeft':
-				iterate(-1);
+				imgIterate(-1);
 				break;
 			case 'ArrowRight':
-				iterate(1);
+				imgIterate(1);
 				break;
 			case 'Escape':
 				document.exitFullscreen();
 				break;
+			case 'Tab':
+				if(e.shiftKey) {
+					albumIterate(-1);
+				} else {
+					albumIterate(1);
+				}
+				imgIdx = 0;
+				return false;
 		}
 	}
 </script>
 
 <div id="lightbox" class="{isFullScreen ? '' : 'hidden'} absolute h-full w-full bg-gray-800">
-	{#if imgs.length > 1}
+	{#if albums[albumIdx].images.length > 1}
 		<div class="absolute inset-y-0 left-0 flex items-center justify-center">
-			<button aria-label="prev" on:click={_ => iterate(-1)} class="icon h-3/4 rounded-r-3xl">
+			<button aria-label="prev" on:click={_ => imgIterate(-1)} class="icon h-3/4 rounded-r-3xl">
 				<svg viewBox="0 -960 960 960">
 					<path d="M400-80 0-480l400-400 71 71-329 329 329 329-71 71Z" />
 				</svg>
 			</button>
 		</div>
 		<div class="absolute inset-y-0 right-0 flex items-center justify-center">
-			<button aria-label="next" on:click={_ => iterate(1)} class="icon h-3/4 rounded-l-3xl">
+			<button aria-label="next" on:click={_ => imgIterate(1)} class="icon h-3/4 rounded-l-3xl">
 				<svg viewBox="0 -960 960 960">
 					<path d="m321-80-71-71 329-329-329-329 71-71 400 400L321-80Z" />
 				</svg>
@@ -51,8 +63,8 @@
 		</div>
 		<div class="absolute inset-x-0 bottom-2 flex items-center justify-center">
 			<div class="flex w-16 fill-white">
-				{#each Array(imgs.length), i}
-					{#if i == idx}
+				{#each albums[albumIdx].images, i}
+					{#if i == imgIdx}
 						<svg viewBox="0 -960 960 960">
 							<path d="M480-280q83 0 141.5-58.5T680-480q0-83-58.5-141.5T480-680q-83 0-141.5 58.5T280-480q0 83 58.5 141.5T480-280Zm0 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/>
 						</svg>
@@ -72,10 +84,10 @@
 			</svg>
 		</button>
 	{/if}
-	<img class="w-full h-full object-scale-down" src={imgs[idx]} alt="img" />
+	<img class="w-full h-full object-scale-down" src={albums[albumIdx].images[imgIdx]} alt="img" />
 </div>
 
-<svelte:window on:keyup|preventDefault={onKeyUp} on:fullscreenchange={_ => isFullScreen = !isFullScreen} />
+<svelte:window on:keydown|preventDefault={onKeyDown} on:fullscreenchange={_ => isFullScreen = !isFullScreen} />
 
 <style lang="postcss">
 	.icon {
